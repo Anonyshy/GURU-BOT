17a2b7625f669d63bda6c474a398fed2b0a68d24
Anonyshy <102345443+Anonyshy@users.noreply.github.com>
2023-07-01 08:53:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94759996966', 'Anony', true],
+  ['94719242999', 'Shyaman', true], 
   [''] 
 ] //Number of owners
 
